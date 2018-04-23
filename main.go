9772c7452db79a760f512f736126ea6456d54d0d
mpppk <niboshiporipori@gmail.com>
2018-04-24 00:06:26 +0900

    Add dammy json api

--- main.go
+++ main.go
@@ -1,10 +1,72 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func main() {
-	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("static"))))
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
+	http.HandleFunc("/json", createJsonHandleFunc(echoBodyFunc))
+	http.ListenAndServe(":8080", nil)
+}
+
+type Input struct {
+	In string
+}
+
+type Output struct {
+	Out string
+}
+
+type ErrorOutput struct {
+	Error error
+}
+
+type BodyFunc func(body []byte) (interface{}, error)
+
+func echoBodyFunc(body []byte) (interface{}, error) {
+	input := Input{}
+	if err := json.Unmarshal(body, &input); err != nil {
+		return nil, err
+	}
+	return input, nil
+}
+
+func createJsonHandleFunc(bodyFunc BodyFunc) http.HandlerFunc {
+	jsonHandleFunc := func(rw http.ResponseWriter, req *http.Request) {
+		var retJson interface{}
+		defer func() {
+			marshaledJson, err := json.Marshal(retJson)
+			if err != nil {
+				log.Fatal(err)
+			}
+			rw.Header().Set("Content-Type", "application/json")
+			fmt.Fprint(rw, string(marshaledJson))
+		}()
+
+		if req.Method != "POST" {
+			retJson = ErrorOutput{errors.New("request is not post method")}
+			return
+		}
+
+		body, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			retJson = ErrorOutput{err}
+			return
+		}
+
+		input, err := bodyFunc(body)
+		if err != nil {
+			retJson = ErrorOutput{err}
+			return
+		}
+		retJson = input
+	}
+	return jsonHandleFunc
 }
