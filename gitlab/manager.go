c43574f80dfd90022a92522241c590f3d867b6a0
mpppk <niboshiporipori@gmail.com>
2018-05-11 15:05:32 +0900

    Add topological sort for work graph

--- gitlab/manager.go
+++ gitlab/manager.go
@@ -1,19 +1,29 @@
 package gitlab
 
 import (
+	"fmt"
 	"sort"
 
+	"gonum.org/v1/gonum/graph"
 	"gonum.org/v1/gonum/graph/simple"
+	"gonum.org/v1/gonum/graph/topo"
 )
 
 type WorkNode struct {
-	node simple.Node
+	node graph.Node
 	work *Work
 }
 
+type Dependency struct {
+	fromIID int
+	toIID   int
+}
+
 type WorkManager struct {
-	g    *simple.DirectedGraph
-	gMap map[int64]*WorkNode
+	g           *simple.DirectedGraph
+	gMap        map[int64]*WorkNode
+	workMap     map[int]*WorkNode
+	depIIDQueue []*Dependency
 }
 
 func NewWorkManager() *WorkManager {
@@ -24,9 +34,29 @@ func NewWorkManager() *WorkManager {
 }
 
 func (wg *WorkManager) AddWork(work *Work) {
-	workNode := wg.g.NewNode()
-	wg.gMap[workNode.ID()] = &WorkNode{}
-	wg.g.AddNode(workNode)
+	node := wg.g.NewNode()
+	wg.gMap[node.ID()] = &WorkNode{node: node, work: work}
+	wg.workMap[work.Issue.IID] = &WorkNode{node: node, work: work}
+	// 依存関係をqueueに入れておく
+	for _, issue := range work.Dependencies.Issues {
+		wg.depIIDQueue = append(wg.depIIDQueue, &Dependency{fromIID: work.Issue.IID, toIID: issue.IID})
+	}
+}
+
+func (wg *WorkManager) ConnectByDependencies() error {
+	for _, dep := range wg.depIIDQueue {
+		fromWorkNode, ok := wg.workMap[dep.fromIID]
+		if !ok {
+			return fmt.Errorf("dependency (from: %s) cant resolve\n", dep) // FIXME
+		}
+		toWorkNode, ok := wg.workMap[dep.toIID]
+		if !ok {
+			return fmt.Errorf("dependency (to: %s) cant resolve\n", dep) // FIXME
+		}
+
+		wg.g.Edge(fromWorkNode.node.ID(), toWorkNode.node.ID())
+	}
+	return nil
 }
 
 func (wg *WorkManager) AddWorks(works []*Work) {
@@ -47,23 +77,34 @@ func (wg *WorkManager) ListSortedWorksByDueDate() (workNodes []*WorkNode) {
 }
 
 func (wg *WorkManager) GetSortedWorks() (works []*Work, err error) {
-	workNodeFlags := map[int64]struct{}{}
-	// TODO: 締め切りが設定されているworkを短い順に取り出す
-	for _, workNodes := range wg.ListSortedWorksByDueDate() {
-		nodes := wg.g.To(workNodes.node.ID())
-		for _, node := range nodes {
-			if _, ok := workNodeFlags[node.ID()]; !ok {
-				continue
-			}
+	nodes, err := topo.Sort(wg.g)
+	if err != nil {
+		return nil, err
+	}
 
-			// TODO: スケジュールに追加する処理
-			// TODO: 取り出されたworkごとに依存ソートして配置
-			// TODO: 締め切りまでの利用可能日数を計算
-			// TODO: 日数が足りなければ並列度を増やす
-
-			workNodeFlags[node.ID()] = struct{}{}
-		}
+	for _, node := range nodes {
+		works = append(works, wg.gMap[node.ID()].work)
 	}
 
 	return
+
+	//workNodeFlags := map[int64]struct{}{}
+	//// TODO: 締め切りが設定されているworkを短い順に取り出す
+	//for _, workNodes := range wg.ListSortedWorksByDueDate() {
+	//	nodes := wg.g.To(workNodes.node.ID())
+	//	for _, node := range nodes {
+	//		if _, ok := workNodeFlags[node.ID()]; !ok {
+	//			continue
+	//		}
+	//
+	//		// TODO: スケジュールに追加する処理
+	//		// TODO: 取り出されたworkごとに依存ソートして配置
+	//		// TODO: 締め切りまでの利用可能日数を計算
+	//		// TODO: 日数が足りなければ並列度を増やす
+	//
+	//		workNodeFlags[node.ID()] = struct{}{}
+	//	}
+	//}
+	//
+	//return
 }
