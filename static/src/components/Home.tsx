3be4efde7927f2366657ee3e622e78f9370e92d5
mpppk <niboshiporipori@gmail.com>
2018-05-12 12:52:17 +0900

    Add refresh button

--- static/src/components/Home.tsx
+++ static/src/components/Home.tsx
@@ -1,5 +1,23 @@
 import * as React from 'react';
 import { ConnectedIssueTable } from './IssueTable';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import AutoRenew from 'material-ui/svg-icons/action/autorenew';
+import { CSSProperties } from 'react';
+
+const style: CSSProperties = {
+  margin: 0,
+  top: 'auto',
+  right: 20,
+  bottom: 20,
+  left: 'auto',
+  position: 'fixed'
+};
+
+const Refresh = () => (
+  <FloatingActionButton style={style}>
+    <AutoRenew />
+  </FloatingActionButton>
+);
 
 export class Home extends React.Component<{}, any> {
   constructor(props: {}) {
@@ -9,6 +27,7 @@ export class Home extends React.Component<{}, any> {
   public render() {
     return (
       <div>
+        <Refresh />
         <ConnectedIssueTable />
       </div>
     );
