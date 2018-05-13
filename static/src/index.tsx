0df4dbea2f8c8454e393899de2062922302961d8
mpppk <niboshiporipori@gmail.com>
2018-05-13 14:23:52 +0900

    Update react-hot-loader

--- static/src/index.tsx
+++ static/src/index.tsx
@@ -1,7 +1,7 @@
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
+import { hot } from 'react-hot-loader';
 
 import { Provider } from 'react-redux';
 import { applyMiddleware, compose, createStore } from 'redux';
@@ -22,29 +22,14 @@ const store = createStore(
 
 sagaMiddleware.run(rootSaga);
 
-ReactDOM.render(
-  <AppContainer>
-    <MuiThemeProvider>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </MuiThemeProvider>
-  </AppContainer>,
-  document.getElementById('example')
+const RootApp = () => (
+  <MuiThemeProvider>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </MuiThemeProvider>
 );
 
-if (module.hot) {
-  module.hot.accept('./components/App', () => {
-    const NextApp = (require('./components/App') as any).default; // tslint:disable-line variable-name
-    ReactDOM.render(
-      <AppContainer>
-        <MuiThemeProvider>
-          <Provider store={store}>
-            <NextApp />
-          </Provider>
-        </MuiThemeProvider>
-      </AppContainer>,
-      document.getElementById('example')
-    );
-  });
-}
+const HotRootApp = hot(module)(RootApp);
+
+ReactDOM.render(<HotRootApp />, document.getElementById('example'));
